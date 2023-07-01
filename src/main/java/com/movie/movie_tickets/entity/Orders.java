c8c4c54427971129225940b5cd328f1603e44e19
Ron Nuriel <ron@nuriell.com>
2023-07-01 12:10:38 +0300

    user_orders Many to Many
    Signed-off-by: Ron Nuriel <ron@nuriell.com>

--- src/main/java/com/movie/movie_tickets/entity/Orders.java
+++ src/main/java/com/movie/movie_tickets/entity/Orders.java
@@ -2,6 +2,8 @@ package com.movie.movie_tickets.entity;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 public class Orders {
     @Id
@@ -13,16 +15,22 @@ public class Orders {
     @JoinColumn(name = "showOccurrence_id")
     private Movie_Occurrence showOccurrence;
 
-    @OneToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+    @ManyToMany
+    @JoinTable(
+            name = "user_orders",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> users;
+
+
     public Orders() {
     }
 
-    public Orders(int orderId, Movie_Occurrence showOccurrence, User user) {
+    public Orders(int orderId, Movie_Occurrence showOccurrence, List<User> users) {
         this.orderId = orderId;
         this.showOccurrence = showOccurrence;
-        this.user = user;
+        this.users = users;
     }
 
     public int getOrderId() {
@@ -41,11 +49,11 @@ public class Orders {
         this.showOccurrence = showOccurrence;
     }
 
-    public User getUser() {
-        return user;
+    public List<User> getUsers() {
+        return users;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    public void setUsers(List<User> users) {
+        this.users = users;
     }
 }
